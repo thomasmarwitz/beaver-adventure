446149e97a0026a4a25a16631540565ed3df1ea9
thomasmarwitz <thomasmarwitz3@gmail.com>
2024-07-11 17:43:46 -0400

    Add details to 1. scene

--- assets.js
+++ assets.js
@@ -24,7 +24,7 @@ const WATER_COLOR = "#4fa4f4";
 const GRASS = {"text": "🌱", "color": BASE_GREEN}
 const LAWN = {"text": "", "color": BASE_GREEN}
 const TREE = {"text": "🌲", "color": BASE_GREEN}
-const WALL = {"text": "", "color": WALL_COLOR}
+const WALL = {"text": "🟫", "color": WALL_COLOR}
 const DOOR = {"text": "🚪", "color": WALL_COLOR}
 const WINDOW = {"text": "🪟", "color": WALL_COLOR}
 const BED = {"text": "🛌", "color": INSIDE_COLOR}
@@ -34,43 +34,44 @@ const LOGS = {"text": "🪵", "color": BASE_GREEN}
 const CORN = {"text": "🌾", "color": "#4d3300"}
 const WATER = {"text": "🌊", "color": WATER_COLOR}
 const PATH = {"text": "", "color": PATH_COLOR}
-const FLOWER = {"text": "🌸", "color": BASE_GREEN}
 const BUSH = {"text": "🌿", "color": BASE_GREEN}
 const ROCK = {"text": "🪨", "color": BASE_GREEN}
-const LOG = {"text": "🌳", "color": BASE_GREEN}
+const LEAF_TREE = {"text": "🌳", "color": BASE_GREEN}
 const SQUIRREL = {"text": "🐿️", "color": BASE_GREEN}
 const CARROT = {"text": "🥕", "color": "#4d3300"}
 const BRICK = {"text": "🧱", "color": BASE_GREEN}
+const WATER_LILY = {"text": "🪷", "color": WATER_COLOR}
+const FISH = {"text": "🐟", "color": WATER_COLOR}
+const COLORFUL_FISH = {"text": "🐠", "color": WATER_COLOR}
 
-// more emoji ideas
-/*
-Garden: 🌷🌹🥀🌺🌸🪻🌼🌻
-Water:
-Fish: 🐟, 🐠 (colorful fish)
-Water Plants: 🌿 (seaweed), 🪷 (lily)
-
-*/
-
+const FLOWER = {"text": "🌸", "color": BASE_GREEN}
+const ROSE = {"text": "🌹", "color": BASE_GREEN}
+const TULIP = {"text": "🌷", "color": BASE_GREEN}
+const DAISY = {"text": "🌼", "color": BASE_GREEN}
+const SUNFLOWER = {"text": "🌻", "color": BASE_GREEN}
+const VIOLET = {"text": "🪻", "color": BASE_GREEN}
 const COW = {"text": "🐄", "color": BASE_GREEN};
+const STATUE = {"text": "🗿", "color": BASE_GREEN};
+const FOUNTAIN = {"text": "⛲", "color": BASE_GREEN};
 
 const HOME_SCENE = new Scene(
     [
-        [TREE, TREE, TREE, TREE, TREE, TREE, TREE, TREE, LAWN, PATH, TREE, TREE, TREE, TREE, TREE, TREE],
-        [TREE, TREE, TREE, TREE, TREE, TREE, TREE, TREE, LAWN, PATH, LOGS, LOGS, LOGS, LOGS, LOGS, LOGS],
+        [TREE, TREE, TREE, LEAF_TREE, TREE, TREE, TREE, TREE, TREE, PATH, TREE, TREE, TREE, TREE, BUSH, TREE],
+        [TREE, TREE, ROCK, TREE, BUSH, TREE, LEAF_TREE, TREE, LAWN, PATH, LOGS, LOGS, LOGS, LOGS, LOGS, LOGS],
         [TREE, TREE, WALL, WALL, WALL, WALL, WALL, WALL, LAWN, PATH, LOGS, LAWN, GRASS, COW, LAWN, LOGS],
-        [TREE, TREE, WALL, INSIDE, INSIDE, INSIDE, INSIDE, WALL, LAWN, PATH, LOGS, GRASS, COW, LAWN, LAWN, LOGS],
-        [TREE, TREE, WALL, INSIDE, INSIDE, INSIDE, INSIDE, WALL, LAWN, PATH, LOGS, LAWN, LAWN, LAWN, COW, LOGS],
-        [TREE, TREE, WALL, INSIDE, INSIDE, INSIDE, INSIDE, WALL, LAWN, PATH, LOGS, LOGS, LOGS, LOGS, LOGS, LOGS],
-        [LAWN, LAWN, WALL, WALL, DOOR, WALL, WALL, WALL, LAWN, PATH, LAWN, LAWN, LAWN, LAWN, LAWN, LAWN],
-        [PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, LAWN, LAWN, LAWN, LAWN, LAWN, LAWN],
-        [TREE, TREE, FLOWER, FLOWER, PATH, LAWN, LAWN, CORN, CORN, CORN, CORN, CARROT, CARROT, CARROT, TREE, TREE],
-        [TREE, TREE, FLOWER, FLOWER, PATH, LAWN, LAWN, CORN, CORN, CORN, CORN, CARROT, CARROT, CARROT, TREE, TREE],
-        [TREE, TREE, BUSH, BUSH, PATH, LAWN, LAWN, CORN, CORN, CORN, CORN, CARROT, CARROT, CARROT, TREE, TREE],
-        [TREE, TREE, BUSH, BUSH, PATH, LAWN, LAWN, CORN, CORN, CORN, CORN, CARROT, CARROT, CARROT, TREE, TREE],
-        [TREE, TREE, TREE, TREE, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, WATER, WATER, WATER, WATER],
-        [TREE, TREE, ROCK, LOG, SQUIRREL, PATH, PATH, LAWN, LAWN, LAWN, LAWN, WATER, WATER, WATER, WATER, WATER],
-        [TREE, TREE, TREE, TREE, TREE, PATH, PATH, LAWN, LAWN, LAWN, LAWN, WATER, WATER, WATER, WATER, WATER],
-        [TREE, TREE, TREE, TREE, TREE, PATH, PATH, LAWN, LAWN, LAWN, LAWN, WATER, WATER, WATER, WATER, WATER],
+        [TREE, LEAF_TREE, WALL, INSIDE, INSIDE, INSIDE, INSIDE, WALL, LAWN, PATH, LOGS, GRASS, COW, LAWN, LAWN, LOGS],
+        [TREE, ROCK, WALL, INSIDE, INSIDE, INSIDE, INSIDE, WALL, LAWN, PATH, LOGS, LAWN, LAWN, LAWN, COW, LOGS],
+        [LEAF_TREE, TREE, WALL, INSIDE, INSIDE, INSIDE, INSIDE, WALL, LAWN, PATH, LOGS, LOGS, LOGS, LOGS, LOGS, LOGS],
+        [TREE, LAWN, WALL, WALL, DOOR, WALL, WALL, WALL, LAWN, PATH, LAWN, CARROT, CARROT, CARROT, CARROT, LAWN],
+        [PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, LAWN, CARROT, CARROT, CARROT, CARROT, LAWN],
+        [TREE, LAWN, LAWN, LAWN, PATH, LAWN, GRASS, VIOLET, LAWN, PATH, PATH, PATH, PATH, PATH, PATH, LAWN],
+        [TREE, LOGS, LAWN, LAWN, PATH, TULIP, FOUNTAIN, GRASS, LAWN, LAWN, LAWN, CORN, CORN, CORN, CORN, LAWN],
+        [TREE, LEAF_TREE, LAWN, LAWN, PATH, LAWN, SUNFLOWER, LAWN, FLOWER, GRASS, LAWN, CORN, CORN, CORN, CORN, LAWN],
+        [TREE, TREE, LAWN, LAWN, PATH, LAWN, LAWN, GRASS, LAWN, DAISY, LAWN, CORN, CORN, CORN, CORN, LAWN],
+        [TREE, ROCK, LAWN, LAWN, PATH, PATH, PATH, PATH, LAWN, LAWN, LAWN, LAWN, WATER, WATER, WATER, WATER],
+        [TREE, SQUIRREL, ROCK, LEAF_TREE, LAWN, LAWN, LAWN, PATH, LAWN, GRASS, LAWN, WATER, WATER, WATER, WATER, COLORFUL_FISH],
+        [TREE, LEAF_TREE, TREE, TREE, TREE, LAWN, LAWN, PATH, GRASS, STATUE, GRASS, WATER, WATER, WATER_LILY, WATER, WATER],
+        [TREE, TREE, TREE, TREE, TREE, BUSH, LAWN, PATH, LAWN, GRASS, LAWN, WATER, WATER, WATER, FISH, WATER],
     ],
     {x: 3, y: 3},
     "Welcome to your cozy beaver lodge! Explore your home, garden, and the surrounding nature."
